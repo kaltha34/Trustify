bbf4f977cbcdfe6cb643b32a70b48c7d34640bf8
bhagya-dewmini <bhagya.20231177@iit.ac.lk>
2025-03-03 19:59:44 +0530

    implement notifications.js file

--- inbox_backend/inbox_backend/routes/notifications.js
+++ inbox_backend/inbox_backend/routes/notifications.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const { getNotifications, addNotification, replyToNotification } = require("../controllers/notificationController");
+
+const router = express.Router();
+
+router.get("/", getNotifications);
+router.post("/", addNotification);
+router.put("/:id/reply", replyToNotification);
+
+module.exports = router;
 
