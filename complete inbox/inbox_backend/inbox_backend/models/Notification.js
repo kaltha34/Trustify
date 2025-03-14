26ffa3a53f956322f8ab5f0b599a09129f1e7d4f
sandaru-hansaja <150640513+sandaru-hansaja@users.noreply.github.com>
2025-03-14 12:26:23 +0530

    Add node_modules to .gitignore and remove from repo

--- complete inbox/inbox_backend/inbox_backend/models/Notification.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const NotificationSchema = new mongoose.Schema({
-  message: { type: String, required: true },
-  reply: { type: String, default: "" },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model("Notification", NotificationSchema);
