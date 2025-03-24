cae759b5b3e8b98e25465f3a4296fe08ebdcea0d
Chamidu Lakshan <151443855+lakshanchamidu@users.noreply.github.com>
2025-03-24 11:39:15 +0530

    Delete necessary files

--- TrustifyFull/backend/models/Insights.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const InsightsSchema = new mongoose.Schema({
-  verifiedUsers: { type: Number, default: 0 },
-  fraudAlerts: { type: Number, default: 0 },
-});
-
-module.exports = mongoose.model("Insights", InsightsSchema);
